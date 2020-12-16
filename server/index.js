3abaa823564511a0008b4286e40f3e0c07494736
asd-developer <andreduarte.profissional@gmail.com>
2020-12-16 15:13:49 +0000

    Added connection url and mongoose

--- server/index.js
+++ server/index.js
@@ -4,8 +4,22 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import { allowedNodeEnvironmentFlags } from 'process';
 
+require('dotenv').config();
+
 const app = express();
+
+
 
 app.use(bodyParser.json({limit: "10mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "10mb", extended: true}));
 app.use(cors())
+
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fraction-tale.hnvgd.mongodb.net/<dbname>?retryWrites=true&w=majority;`
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=> app.listen(PORT,()=> console.log(`Server running on PORT: ${PORT}`)))
+.catch((error)=> console.log(error.message));
+
+mongoose.set('useFindAndModify', false)
