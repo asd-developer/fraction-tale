bf1658c194f1dd7ebcd0b4eaa77d2f2fb919b294
asd-developer <andreduarte.profissional@gmail.com>
2020-12-16 12:43:21 +0000

    Added dotenv to server

--- server/index.js
+++ server/index.js
@@ -2,4 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import { allowedNodeEnvironmentFlags } from 'process';
 
+const app = express();
+
+app.use(bodyParser.json({limit: "10mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "10mb", extended: true}));
+app.use(cors())
