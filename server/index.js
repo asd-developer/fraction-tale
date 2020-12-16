bcf710587003195bd93ae40bca13ad89b5d68e5a
asd-developer <andreduarte.profissional@gmail.com>
2020-12-16 19:08:59 +0000

    dotenv now working properly

--- server/index.js
+++ server/index.js
@@ -5,18 +5,15 @@ import cors from 'cors';
 import dotenv from 'dotenv'
 
 const app = express();
-
-
+dotenv.config();
 
 app.use(bodyParser.json({limit: "10mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "10mb", extended: true}));
 app.use(cors());
 
-const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fraction-tale.hnvgd.mongodb.net/<dbname>?retryWrites=true&w=majority`
-
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=> app.listen(PORT,()=> console.log(`Server running on PORT: ${PORT}`)))
 .catch((error)=> console.log(error.message));
 
