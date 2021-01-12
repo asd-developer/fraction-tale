4b55fe839db5e59b4be54384baf4979a5eb74304
asd-developer <andreduarte.profissional@gmail.com>
2021-01-12 14:27:25 +0000

    Added register form

--- client/src/components/RegisterPage/RegisterPage.js
+++ client/src/components/RegisterPage/RegisterPage.js
@@ -6,6 +6,13 @@ const RegisterPage = () => {
     <>
         <div>
             Hello ola
+            <form onSubmit={""}>
+              <input type="text" placeholder="name"></input>
+              <input type="text" placeholder="email"></input>
+              <input type="text" placeholder="password"></input>
+              <input type="text" placeholder="confirm password"></input>
+              <button>Submit</button>
+            </form>
         </div>  
     </>
   );
