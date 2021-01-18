a72bf75d65672d2ff06eda4df8a152215e04aae4
asd-developer <andreduarte.profissional@gmail.com>
2021-01-18 13:52:51 +0000

    Improved user Schema

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,10 +1,23 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    username: {type: String, required: true, minlength: 3},
-    email: {type: String, required: true},
-    password: {type: String, required: true, minlength: 5},
-});
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        trim:true,
+        minlength: 3
+    },
+    email: {
+        type: String, 
+        required: true
+    },
+    password: {
+        type: String, 
+        required: true, 
+        minlength: 5
+    },
+},{timestamps: true,});
 
 const  User = mongoose.model("user", userSchema);
 export default User;
