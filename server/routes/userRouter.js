03b30f8685fe7a2f580a789e96e8096238a535af
asd-developer <andreduarte.profissional@gmail.com>
2021-01-18 14:02:52 +0000

    Users can now be added to MongoDB

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,28 @@
 import express from 'express';
+import User from '../models/userModel.js';
 
 const router = express.Router();
 
 router.get("/test", (req,res) =>{
     res.send("Hello it's working")
 })
+
+router.route('/add').post((req,res)=>{
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const newUser = new User(
+        {
+            username,
+            email,
+            password,
+        });
+
+    newUser.save()
+        .then(() => res.json('User added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
 
 export default  router
