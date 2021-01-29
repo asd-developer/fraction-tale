2d5f036a230182bf59ce74fcef23027b967b55bf
asd-developer <andreduarte.profissional@gmail.com>
2021-01-29 21:40:04 +0000

    Added register pages but changes are required

---
+++ fraction-tale/pages/register/RegisterPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Container, Form } from './RegisterPage.elements'
+
+const RegisterPage = () => {
+  return (
+    <>
+        <Container>
+            <h1>Register</h1>
+            <Form onSubmit={""}>
+              <input type="text" placeholder="username"></input>
+              <input type="text" placeholder="email"></input>
+              <input type="text" placeholder="password"></input>
+              <input type="text" placeholder="confirm password"></input>
+              <button>Submit</button>
+            </Form>
+        </Container>  
+    </>
+  );
+}
+
+export default RegisterPage;
