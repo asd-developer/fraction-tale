74205c76d1469c31a13221ab55a7923c42fbde1b
asd-developer <andreduarte.profissional@gmail.com>
2021-02-04 16:03:18 +0000

    Created form handlers for Navbar login

--- fraction-tale/src/components/navbar/Navbar.js
+++ fraction-tale/src/components/navbar/Navbar.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import {React, useState} from 'react';
 import styles from './Navbar.module.scss'
 
 const Navbar = () => {
+//STATE
+  //LOGIN FORM STATE
+  const [getLoginState, setLoginState] = useState({
+    username: '',
+    password: '',
+  })
+
+//HELPER METHODS
+  //RUNS ON FORM SUBMISSION
+  const submitFormHandler = (event) =>{
+    event.preventDefault();
+  }
+
+  //HANDLES CHANGE TO THE USERNAME AND PASSWORD INPUTS
+  const userHandler = (event) =>{
+    setLoginState({...getLoginState, [event.target.name] : event.target.value});
+  }
+
   return (
       <nav className={styles.sticky_navbar}>
           <div className={styles.flexstart}>
               <a href="/"><h1 className={styles.page_title} href="/">Fraction Tale</h1></a>
           </div>
           <div className={styles.flexend}>
-              <input className={styles.login} type="text" placeholder="Username"/>
-              <input className={styles.password}  type="password" placeholder="Password"/>
-                <a href="/login">
-                  <button className={styles.login_bt}>Enter</button>
-                </a>
-                <a href="/register">
+            <form onSubmit={submitFormHandler}>
+              <input className={styles.login} type="text" placeholder="Username" name="username" onChange={userHandler}/>
+              <input className={styles.password}  type="password" placeholder="Password" name="password" onChange={userHandler}/>
+                <button type="submit" className={styles.login_bt}>Enter</button>
+              <a href="/register">
                   <button className={styles.register_bt}>Register</button> 
-                </a>
+              </a>
+            </form>
           </div>
       </nav>
   );
